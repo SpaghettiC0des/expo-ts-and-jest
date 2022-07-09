077e99dacd664038d3a594fe30a3f77ee57b8992
Karl Marx Lopez <karlmarx.webdev@gmail.com>
2022-07-10 01:07:10 +0800

    fix: check if we are not running inside Jest environment

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import 'react-native-gesture-handler';
 
-if (__DEV__) {
+// Make sure we are not running inside Jest environment
+if (!process.env.JEST_WORKER_ID && __DEV__) {
   require('react-devtools-core').connectToDevTools({
     host: 'localhost',
     port: 8097,
